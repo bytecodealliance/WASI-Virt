79655e906e18a21fba181df128317627af10a849
Guy Bedford <gbedford@fastly.com>
2023-09-01 13:25:22 -0700

    fixup

--- virtual-adapter/src/io.rs
+++ virtual-adapter/src/io.rs
@@ -44,7 +44,6 @@ use crate::wasi::clocks::monotonic_clock;
 use crate::wasi::http::types as http_types;
 use crate::wasi::poll::poll;
 use crate::wasi::sockets::ip_name_lookup;
-use crate::wasi::sockets::network;
 use crate::wasi::sockets::tcp;
 use crate::wasi::sockets::udp;
 
@@ -1518,7 +1517,7 @@ impl IpNameLookup for VirtAdapter {
         name: String,
         address_family: Option<IpAddressFamily>,
         include_unavailable: bool,
-    ) -> Result<ip_name_lookup::ResolveAddressStream, network::ErrorCode> {
+    ) -> Result<ip_name_lookup::ResolveAddressStream, NetworkErrorCode> {
         ip_name_lookup::resolve_addresses(network, &name, address_family, include_unavailable)
     }
     fn resolve_next_address(
