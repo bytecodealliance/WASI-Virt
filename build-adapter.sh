1d355fa47edad9be20eba3541ea6afca633e6d64
Victor Adossi <vadossi@cosmonic.com>
2025-03-27 01:26:15 +0900

    refactor: build-adapter script
    Signed-off-by: Victor Adossi <vadossi@cosmonic.com>

--- build-adapter.sh
+++ build-adapter.sh
@@ -5,8 +5,19 @@ export RUSTFLAGS="-Zoom=panic"
 
 wasm-tools component wit --wasm wit -o lib/package.wasm
 
-cargo build -p virtual-adapter --target wasm32-unknown-unknown --release -Z build-std=std,panic_abort -Z build-std-features=panic_immediate_abort &&
-    cp target/wasm32-unknown-unknown/release/virtual_adapter.wasm lib/virtual_adapter.wasm
+cargo build \
+      -p virtual-adapter \
+      --release \
+      --target wasm32-unknown-unknown \
+      -Z build-std=std,panic_abort \
+      -Z build-std-features=panic_immediate_abort
+cp target/wasm32-unknown-unknown/release/virtual_adapter.wasm lib/virtual_adapter.wasm
 
-cargo build -p virtual-adapter --target wasm32-unknown-unknown --release --features debug -Z build-std=std,panic_abort -Z build-std-features=panic_immediate_abort &&
-    cp target/wasm32-unknown-unknown/release/virtual_adapter.wasm lib/virtual_adapter.debug.wasm
+cargo build \
+      -p virtual-adapter \
+      --release \
+      --target wasm32-unknown-unknown \
+      --features debug \
+      -Z build-std=std,panic_abort \
+      -Z build-std-features=panic_immediate_abort
+cp target/wasm32-unknown-unknown/release/virtual_adapter.wasm lib/virtual_adapter.debug.wasm
